f94805d66c2a73080e5077b547496a8ed95d9974
Kathy Seol <kseol@merkleinc.com>
2023-04-12 14:23:00 -0400

    updated to have good name for intention

--- ui.apps/src/main/content/jcr_root/apps/zoo/clientlibs/clientlibs-global-functions/scripts/global.js
+++ ui.apps/src/main/content/jcr_root/apps/zoo/clientlibs/clientlibs-global-functions/scripts/global.js
@@ -6,19 +6,19 @@
     let registry = $window.adaptTo("foundation-registry");
     if(registry != null || registry != undefined) {
         registry.register("foundation.adapters", {
-            type: "test-adapt",
-            selector: "coral-multifield",
+            type: "limit-multifield",
+            selector: "coral-multifield", //this selector is in reference to the literal element <coral-multifield></coral-multifield>
             adapter: function(el) {
                 let $multifield = $(el);
                 let $addButton = $multifield.children('._coral-Button._coral-Button--primary');
                 console.log("successfully adapted the object");
                 return {
+                    //exposes "limit-multifield" APIs of "coral-multifield" literal element
                     callConsole: function() {
                         console.log("successfully called the callConsole API");
                         return;
                     },
                     disableAdd: function(disabled) {
-                        //let $addButton = $multifield.children('.coral-Multifield-add');
                         $addButton.attr('disabled', disabled);
                     }
                 }
@@ -29,11 +29,13 @@
     $.validator.register("foundation.validation.validator", {
         selector: "coral-multifield",
         validate: function(el) {
-            var $this = $(el);
-            var totalPanels = el["0"].items.getAll().length;
-            var min;
-            var max;
-            let testAdapt = $this.adaptTo("test-adapt");
+            let $this = $(el);
+            let limitMultifield = $this.adaptTo("limit-multifield");
+
+            let totalPanels = el["0"].items.getAll().length;
+            let min;
+            let max;
+
             if ($this.data("min-item")){
                 min = $this.data("min-item");
                 if(totalPanels < min) {
@@ -42,13 +44,13 @@
             }
             if ($this.data("max-item")){
                 max = $this.data("max-item");
+                limitMultifield.disableAdd(false);
                 if(totalPanels >= max) {
-                    testAdapt.callConsole();
-                    testAdapt.disableAdd(true);
+                    limitMultifield.callConsole();
+                    limitMultifield.disableAdd(true);
                     return "Maximum numbers of items allowed are: " + max;
                 }
             }
-
         }
     });
 
